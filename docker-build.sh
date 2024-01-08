88ce29dcb43ee84c553363828351a2b81d4ae42b
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2024-01-07 19:17:05 -0600

    Build system updates

--- docker-build.sh
+++ docker-build.sh
@@ -13,6 +13,8 @@ cd adt-pulse-mqtt
 
 #docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/adt-pulse-mqtt .
 
-docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/adt-pulse-mqtt .
+#docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/adt-pulse-mqtt .
+
+docker build -t local/adt-pulse-mqtt .
 
 
